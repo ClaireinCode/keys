a85a6d5eab30a0022d7c29585d21a7f710195ddb
Claireese Underwood <claireese.underwood@gmail.com>
2023-12-22 10:52:32 -0600

    updated design for thoughts page

--- frontend/keys_proj/src/pages/ProfileThoughtsPage.jsx
+++ frontend/keys_proj/src/pages/ProfileThoughtsPage.jsx
@@ -47,11 +47,13 @@ const ProfileThoughtsPage = () => {
     return (
         <>
         <div id="thoughts_page">
+        <div id="outer_thoughts">
         <div id="personal_thoughts"><h4>Your Thoughts</h4></div>
             {allThoughts.length > 0 ? (allThoughts.map((thought, index) => (
-                <div key={thought.id} className="thoughts_div"><h5>{thought.house_id}</h5>{thought.thoughts}<button onClick={() => deleteThought(thought.id)}>Trash</button></div>))
+                <div key={thought.id} className="thoughts_div"><div id="inner_thoughts"><h5>{thought.house_id}</h5>{thought.thoughts}</div><img onClick={() => deleteThought(thought.id)} id="thoughts_delete" src="https://imgs.search.brave.com/1pXk6Ir8d98XmoKLhjh6IvWlvJ0Yk65D3HrBPm6YKPU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvZXNzZW50aWFs/LTIxLzEyOC9UcmFz/aC0xMjgucG5n"/></div>))
             ):( <div className="thoughts_div">No thoughts yet!</div>)}
         <div id="thought_create_div"></div>
+        </div>
         </div>
         </>
     )
